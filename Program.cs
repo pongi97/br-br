5c3c094bd8a50ca6cfb9598baea77fa5cdbd1903
Filip Pongrac <44971791+pongi97@users.noreply.github.com>
2018-12-10 12:33:43 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Razlomak r1 = new Razlomak(2, 5);
+
+            Razlomak r2 = new Razlomak(4, 0);
+
+            Console.WriteLine("Razlomak 1  {0} / {1} = {2}, razlomak ispravan: {3}", r1.VratiBrojnik(), r1.VratiNazivnik(), r1.Pretvoriufloat(), r1.Ispravan());
+
+            Console.WriteLine("Razlomak 2  {0} / {1} = {2}, razlomak ispravan: {3}", r2.VratiBrojnik(), r2.VratiNazivnik(), r2.Pretvoriufloat(), r2.Ispravan());
+
+            Razlomak r3 = new Razlomak(12, 16);
+            r3.SkratiRazlomak();
+            Console.WriteLine("Razlomak 3  {0} / {1} = {2}, razlomak ispravan: {3}", r3.VratiBrojnik(), r3.VratiNazivnik(), r3.Pretvoriufloat(), r3.Ispravan());
+
+            List<Razlomak> razlomci = new  List<Razlomak>();
+            razlomci.Add(r1);
+            razlomci.Add(r2);
+            razlomci.Add(r3);
+            razlomci.Sort();
+            //ispis po velicini
+            Console.Write("Ispis po velicini: ");
+
+            foreach (Razlomak r in razlomci)
+                Console.Write(r.Pretvoriufloat() + ", ");
+        }
+
+    }
+    
+}
---
+++ Razlomak.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Razlomak : IComparable<Razlomak>
+    {
+        private int brojnik, nazivnik;
+
+        public Razlomak(int brojnik, int nazivnik)
+        {
+            this.brojnik = brojnik;
+            this.nazivnik = nazivnik;
+
+
+         }
+        public void ZadajBrojnik(int brojnik)
+        {
+            this.brojnik = brojnik;
+
+        }
+        public void ZadajNazivnik(int nazivnik)
+        {
+            this.nazivnik = nazivnik;
+
+        }
+        public int VratiNazivnik()
+        {
+            return nazivnik;
+        }
+        public int VratiBrojnik()
+        {
+            return brojnik;
+
+        }
+        public float Pretvoriufloat()
+        {
+            return (float)brojnik / nazivnik;
+        }
+        public bool Ispravan()
+        {
+            return nazivnik != 0;
+           /* if(nazivnik!=0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }*/
+        }
+        private int Nzm(int brojnik,int nazivnik)
+        {
+            int temp;
+            while(nazivnik!=0)
+            {
+                temp = nazivnik;
+                nazivnik = brojnik % nazivnik;
+                brojnik=temp;
+            }
+            return brojnik;
+
+        }
+        public void SkratiRazlomak()
+        {
+            if(brojnik<nazivnik)
+            {
+                int mjera= Nzm(nazivnik, brojnik);
+                brojnik = brojnik / mjera;
+                nazivnik = nazivnik / mjera;
+            }
+            else
+            {
+                int mjera = Nzm(brojnik, nazivnik);
+                brojnik = brojnik / mjera;
+                nazivnik = nazivnik / mjera;
+            }
+            
+
+            }
+
+        public int CompareTo(Razlomak other)
+        {
+            //throw new NotImplementedException();
+            if (Pretvoriufloat() > other.Pretvoriufloat())
+            {
+                return 1;
+            }
+            else if (Pretvoriufloat() < other.Pretvoriufloat())
+            {
+                return -1;
+            }
+            else
+            return 0;
+        }
+    }
+}
